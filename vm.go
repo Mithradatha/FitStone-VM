a5656d69060f22b0cadd187adc37e7ee9437363b
Mithradatha <sam.kellar@outlook.com>
2016-08-25 21:25:02 -0400

    Updated .gitignore and added command-line arguments to package

--- vm.go
+++ vm.go
@@ -1,7 +1,26 @@
 package main
 
+import "os"
 import "fmt"
 
+type Platter struct {
+	value uint32
+}
+
+type Register struct {
+	platter Platter
+}
+
 func main() {
-    fmt.Println("Hello World")
+
+	var inputFileName string
+
+	if len(os.Args) > 1 {
+		inputFileName = os.Args[1]
+	}
+
+	outputFileName := "sandmark-output.txt"
+	fmt.Println(inputFileName)
+	fmt.Println(outputFileName)
+
 }
