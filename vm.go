d1a3933877bc1a928aaf00dcd0687849159b4a74
Mithradatha <sam.kellar@outlook.com>
2016-09-05 19:06:32 -0400

    Added AddressStack

--- vm.go
+++ vm.go
@@ -5,72 +5,162 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"bufio"
+	"errors"
+	"strconv"
 )
 
-// TODO: Add channels
+const maxUint32 = 4294967295
+const maxUint8 = 255
+const increment = 256
+const stackSize = 562
+const registerSize = 8
+
+type AddressStack struct {
+    unallocatedMemory chan uint32
+    maxVal chan uint32
+}
+
+func (o *AddressStack) Init() {
+    o.unallocatedMemory = make(chan uint32, stackSize)
+    o.maxVal = make(chan uint32, 1)
+    
+    for i, j := uint32(0), uint32(increment); i < j; i++ {
+        o.unallocatedMemory <- i
+    }
+
+    o.maxVal <- increment
+}
+
+func (o *AddressStack) Print() {
+    
+    close(o.unallocatedMemory)
+    close(o.maxVal)
+
+    fmt.Println("Largest Address: ", <- o.maxVal)
+    fmt.Println("\nUnallocated Addresses:")
+
+    for elem := range o.unallocatedMemory {
+        fmt.Println(elem)
+    }
+}
+
+func (o *AddressStack) populate() error {
+    i := <- o.maxVal
+    j := i + increment
+    
+    if j > maxUint32 {
+        return errors.New("Memory Full")
+    }
+
+    for ; i < j; i++ {
+        o.unallocatedMemory <- i
+    }
+    o.maxVal <- j
+    return nil
+}
+
+func (o *AddressStack) Push(elem uint32) {
+    if len(o.unallocatedMemory) == stackSize {
+        o.Pop()
+    }
+    o.unallocatedMemory <- elem
+}
+
+func (o *AddressStack) Pop() (elem uint32, err error) {
+    elem = <- o.unallocatedMemory
+    if len(o.unallocatedMemory) < 50 {
+        err = o.populate()
+    }
+    return
+}
+
 func main() {
 
-	arrays := map[uint32][]uint32{}
+	output, _ := os.Create("output.txt")
+
 	var executionFinger uint32
-	registers := [8]uint32{}
-	fmt.Println(registers, "   -   ", arrays, executionFinger)
+
+	addresses := new(AddressStack)
+	arrays := map[uint32][]uint32{}
+	registers := [registerSize]uint32{}
+	
+	addresses.Init()
 
 	if len(os.Args) > 1 {
+		
 		filename := os.Args[1]
 		filePtr, err := os.Open(filename)
+		
 		if err == nil {
+			
 			defer filePtr.Close()
+			
 			buffer := make([]byte, 4)
 			var programs []uint32
+			
 			for nBytes, err := filePtr.Read(buffer); nBytes != 0 && err == nil; nBytes, err = filePtr.Read(buffer) {
-				//fmt.Printf("%b   %d   %d\n", buffer, binary.BigEndian.Uint32(buffer), binary.BigEndian.Uint32(buffer)>>28)
 				programs = append(programs, binary.BigEndian.Uint32(buffer))
 			}
-			arrays[0] = programs
 
-			// TODO: cycle()
-			program := arrays[0][executionFinger]
-			operator := program >> 28
-			registerA := (program & 0x1FF) >> 6
-			registerB := (program & 0x3F) >> 3
-			registerC := (program & 0x7) // number & (2^nBits - 1)
-			fmt.Printf("%d  %d, %d, %d\n", operator, registerA, registerB, registerC)
-			fmt.Println(len(programs))
-			executionFinger++
-			switch operator {
-			case 0: // conditionalMove
-				if registers[registerC] != 0 {
-					registers[registerA] = registers[registerB]
+			index0, _ := addresses.Pop()
+			arrays[index0] = programs
+
+			// cycle
+			for isRunning := true; isRunning; {
+				program := arrays[0][executionFinger]
+				operator := program >> 28
+				
+				registerA := (program & 0x1FF) >> 6
+				registerB := (program & 0x3F) >> 3
+				registerC := (program & 0x7) // number & (2^nBits - 1)
+				
+				//fmt.Printf("%d  %d, %d, %d\n", operator, registerA, registerB, registerC)
+				//fmt.Println(len(programs))
+				executionFinger++
+				switch operator {
+				case 0: // conditionalMove
+					if registers[registerC] != 0 {
+						registers[registerA] = registers[registerB]
+					}
+				case 1: // arrayIndex
+					registers[registerA] = arrays[registers[registerB]][registers[registerC]]
+				case 2: // arrayAmendment
+					arrays[registers[registerA]][registers[registerB]] = registers[registerC]
+				case 3: // addition
+					registers[registerA] = uint32(math.Mod(float64(registers[registerB]+registers[registerC]), math.Pow(2, 32)))
+				case 4: // multiplication
+					registers[registerA] = uint32(math.Mod(float64(registers[registerB]*registers[registerC]), math.Pow(2, 32)))
+				case 5: // division
+					if registers[registerC] != 0 {
+						registers[registerA] = uint32(registers[registerB] / registers[registerC])
+					}
+				case 6: // notAnd
+					registers[registerA] = ^(registers[registerB] & registers[registerC])
+				case 7: // halt
+					isRunning = false
+				case 8: // allocation
+					address, _ := addresses.Pop()
+					arrays[address] = make([]uint32, registers[registerC])
+					registers[registerB] = address
+				case 9: // abandonment
+					address := registers[registerC]
+					delete(arrays, address)
+					addresses.Push(address)
+				case 10: // output
+					fmt.Fprintf(output, "%v", string(registers[registerC]))
+				case 11: // input
+					input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+					intStr, _ := strconv.ParseUint(input, 10, 32)
+					registers[registerC] = uint32(intStr)
+				case 12: // loadProgram
+					arr := arrays[registers[registerB]]
+					arrays[0] = arr
+					executionFinger = registers[registerC]
+				case 13: // orthography
+					registers[((program >> 25) & 0x7)] = (program & 0x1FFFFFF)
 				}
-			case 1: // arrayIndex
-				registers[registerA] = arrays[registers[registerB]][registers[registerC]]
-			case 2: // arrayAmendment
-				arrays[registers[registerA]][registers[registerB]] = registers[registerC]
-			case 3: // addition
-				registers[registerA] = uint32(math.Mod(float64(registers[registerB]+registers[registerC]), math.Pow(2, 32)))
-			case 4: // multiplication
-				registers[registerA] = uint32(math.Mod(float64(registers[registerB]*registers[registerC]), math.Pow(2, 32)))
-			case 5: // division
-				if registers[registerC] != 0 {
-					registers[registerA] = uint32(registers[registerB] / registers[registerC])
-				}
-			case 6: // notAnd
-				registers[registerA] = ^(registers[registerB] & registers[registerC])
-			case 7: // halt
-				// TODO: Throw exception or alter isRunning bool to break cycle()
-			case 8: // allocation
-
-			case 9: // abandonment
-
-			case 10: // output
-
-			case 11: // input
-
-			case 12: // loadProgram
-
-			case 13: // orthography
-
-			}
+			}	
 		}
 	}
 }
