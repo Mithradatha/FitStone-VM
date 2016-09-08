988ab4dc5cfd4fa7075f3788ba8ee5face8dccda
Mithradatha <sam.kellar@outlook.com>
2016-09-08 19:05:36 -0400

    Working Version 1.4

--- vm.go
+++ vm.go
@@ -8,24 +8,25 @@ import (
 	"bufio"
 	"errors"
 	"strconv"
-	"time"
+	// "time"
 )
 
 const maxUint32 = 4294967295
 const maxUint8 = 255
-const increment = 256
+const increment = 256	// when the address stack is holding less than 50 elements, populate it with n values
 const stackSize = 562
 const registerSize = 8
 
 type AddressStack struct {
-    unallocatedMemory chan uint32
-    maxVal chan uint32
+    unallocatedMemory chan uint32	// buffered
+    maxVal chan uint32	// buffered
 }
 
 func (o *AddressStack) Init() {
     o.unallocatedMemory = make(chan uint32, stackSize)
     o.maxVal = make(chan uint32, 1)
     
+	// initialize
     for i, j := uint32(0), uint32(increment); i < j; i++ {
         o.unallocatedMemory <- i
     }
@@ -33,6 +34,7 @@ func (o *AddressStack) Init() {
     o.maxVal <- increment
 }
 
+/*
 func (o *AddressStack) Print() {
     
     close(o.unallocatedMemory)
@@ -45,6 +47,7 @@ func (o *AddressStack) Print() {
         fmt.Println(elem)
     }
 }
+*/
 
 func (o *AddressStack) populate() error {
     i := <- o.maxVal
@@ -77,18 +80,20 @@ func (o *AddressStack) Pop() (elem uint32, err error) {
 }
 
 func main() {
-	start := time.Now()
+	
+	// start := time.Now()
+
 	output, _ := os.Create("sandmark-output.txt")
 
-	var executionFinger uint32
+	var executionFinger uint32	// instruction pointer
 
-	addresses := new(AddressStack)
+	addresses := new(AddressStack)	// unallocated addresses
 	arrays := map[uint32][]uint32{}
 	registers := [registerSize]uint32{}
 	
 	addresses.Init()
 
-	if len(os.Args) > 1 {
+	if len(os.Args) > 1 {	// if an argument is provided
 		
 		filename := os.Args[1]
 		filePtr, err := os.Open(filename)
@@ -100,73 +105,80 @@ func main() {
 			buffer := make([]byte, 4)
 			var programs []uint32
 			
+			// while not EOF: read in 4 bytes at a time, convert them to uint32, and store them in the programs slice
 			for nBytes, err := filePtr.Read(buffer); nBytes != 0 && err == nil; nBytes, err = filePtr.Read(buffer) {
 				programs = append(programs, binary.BigEndian.Uint32(buffer))
 			}
 
-			index0, _ := addresses.Pop()
+			// initialize 'zero' array
+			index0, _ := addresses.Pop()	
 			arrays[index0] = programs
+
+			inputBuffer := bufio.NewReader(os.Stdin)
 
 			// cycle
 			for isRunning := true; isRunning; {
 				
 				program := arrays[0][executionFinger]
-				operator := program >> 28
-				
-				registerA := (program & 0x1FF) >> 6
-				registerB := (program & 0x3F) >> 3
-				registerC := (program & 0x7) // number & (2^nBits - 1)
-				
-				executionFinger++
+				operator := program >> 28	// first four bits
 
-				switch operator {
-				case 0: // conditionalMove
-					if registers[registerC] != 0 {
-						registers[registerA] = registers[registerB]
-					}
-				case 1: // arrayIndex
-					registers[registerA] = arrays[registers[registerB]][registers[registerC]]
-				case 2: // arrayAmendment
-					arrays[registers[registerA]][registers[registerB]] = registers[registerC]
-				case 3: // addition
-					registers[registerA] = uint32(math.Mod(float64(registers[registerB]+registers[registerC]), math.Pow(2, 32)))
-				case 4: // multiplication
-					registers[registerA] = uint32(math.Mod(float64(registers[registerB]*registers[registerC]), math.Pow(2, 32)))
-				case 5: // division
-					registers[registerA] = uint32(registers[registerB] / registers[registerC])
-				case 6: // notAnd
-					registers[registerA] = ^(registers[registerB] & registers[registerC])
-				case 7: // halt
-					isRunning = false
-				case 8: // allocation
-					address, _ := addresses.Pop()
-					arrays[address] = make([]uint32, registers[registerC])
-					registers[registerB] = address
-				case 9: // abandonment
-					address := registers[registerC]
-					delete(arrays, address)
-					addresses.Push(address)
-				case 10: // output
-					fmt.Fprintf(output, "%v", string(registers[registerC]))
-				case 11: // input
-					input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-					intStr, _ := strconv.ParseUint(input, 10, 32)
-					registers[registerC] = uint32(intStr)
-				case 12: // loadProgram
-					if registers[registerB] != 0 {
-						arr := arrays[registers[registerB]]
-						tmp := make([]uint32, len(arr))
-						copy(tmp, arr)
-						arrays[0] = tmp
-					}
-					executionFinger = registers[registerC]
-				case 13: // orthography
+				executionFinger++	// move the instruction pointer to the next instruction
+				
+				if operator == 13 {	// orthography
 					registers[((program >> 25) & 0x7)] = (program & 0x1FFFFFF)
+				} else {
+				
+					registerA := (program & 0x1FF) >> 6	// number & (2^nBits - 1)
+					registerB := (program & 0x3F) >> 3
+					registerC := (program & 0x7)	// last three bits
+
+					switch operator {
+					case 0: // conditionalMove
+						if registers[registerC] != 0 {
+							registers[registerA] = registers[registerB]
+						}
+					case 1: // arrayIndex
+						registers[registerA] = arrays[registers[registerB]][registers[registerC]]
+					case 2: // arrayAmendment
+						arrays[registers[registerA]][registers[registerB]] = registers[registerC]
+					case 3: // addition
+						registers[registerA] = uint32(math.Mod(float64(registers[registerB]+registers[registerC]), math.Pow(2, 32)))
+					case 4: // multiplication
+						registers[registerA] = uint32(math.Mod(float64(registers[registerB]*registers[registerC]), math.Pow(2, 32)))
+					case 5: // division
+						registers[registerA] = uint32(registers[registerB] / registers[registerC])
+					case 6: // notAnd
+						registers[registerA] = ^(registers[registerB] & registers[registerC])
+					case 7: // halt
+						isRunning = false
+					case 8: // allocation
+						address, _ := addresses.Pop()
+						arrays[address] = make([]uint32, registers[registerC])
+						registers[registerB] = address
+					case 9: // abandonment
+						address := registers[registerC]
+						delete(arrays, address)
+						addresses.Push(address)
+					case 10: // output
+						fmt.Fprintf(output, "%v", string(registers[registerC]))
+					case 11: // input
+						input, _ := inputBuffer.ReadString('\n')
+						intStr, _ := strconv.ParseUint(input, 10, 32)
+						registers[registerC] = uint32(intStr)
+					case 12: // loadProgram
+						if registers[registerB] != 0 {
+							arr := arrays[registers[registerB]]
+							tmp := make([]uint32, len(arrays[registers[registerB]]))
+							copy(tmp, arr)
+							arrays[0] = tmp
+						}
+						executionFinger = registers[registerC]
+					}
 				}
 			}	
 		}
 	}
-	fmt.Println("Start: ", start)
-	fmt.Println("End: ", time.Now())
-	fmt.Println("Duration: ", time.Since(start))
+	// fmt.Println("Start: ", start)
+	// fmt.Println("End: ", time.Now())
+	// fmt.Println("Duration: ", time.Since(start))
 }
